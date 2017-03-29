151792118f68a179c207213af8195fecbc210dc3
Vadim Nicolai <nicolai.vadim@gmail.com>
2017-03-29 18:19:01 +0300

    GSD: working version.

--- get-shit-done.js
+++ get-shit-done.js
@@ -3,38 +3,96 @@
 
 const fs = require('fs');
 const readline = require('readline');
+const exec = require('child_process').exec;
 
 const hostsFile = '/etc/hosts';
-const startToken = '## start-gsd\n';
+const startToken = '## start-gsd';
 const endToken = '## end-gsd';
-let hostComments = '';
+let hostsFileLines = [];
+let hostLines = '';
+let checkFirstComm = false;
 
 const rl = readline.createInterface({
   input: fs.createReadStream(hostsFile)
 });
 
-rl.on('line', (line) => {
-	if (line === endToken) {
-	  console.log(`Line from file: ${line}`);
-	}
-});
 
-fs.readFile('sites.ini', 'utf8', function (err,data) {
-  if (err) throw err;
-  const sites = data.match(/[^=]+$/)[0].split(', ');
-  
-  hostComments += startToken;
-  
-  for (let site of sites) {  	
-  	hostComments += `127.0.0.1 ${site}\n`;
-  	hostComments += `127.0.0.1 www.${site}\n`;
-  }
+var param = process.argv[2];
 
-  hostComments += endToken;
+function gsd (param) {
+    return param === 'work' ? addToHosts() : param === 'play' ? removeFromHosts() : console.log('Please insert an argument');
+}
 
-	fs.appendFile('message.txt', hostComments, function (err) {
-		console.log(hostComments);
-	  if (err) throw err;
-	  console.log('Saved!');
-	});
-});
+function restartNetwork() {
+    if (process.platform === 'linux') {
+        exec('/etc/init.d/networking restart', (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.log(`stdout: ${stdout}`);
+
+          if (stderr) console.log(`stderr: ${stderr}`);
+        });
+    }
+}
+
+function addToHosts() {
+    rl.on('line', (line) => {
+        hostsFileLines.push(line);
+    });
+
+    rl.on('close', (line) => {
+        if (hostsFileLines[hostsFileLines.length - 1] == endToken) {
+            return console.log('You have already appended to hosts file');
+        }
+        writeToHosts();
+        restartNetwork();
+    });
+}
+
+function removeFromHosts() {
+    rl.on('line', (line) => {
+        if (line === startToken) {
+            checkFirstComm = true;
+        }
+        if (!checkFirstComm) {
+            hostsFileLines.push(line);
+        }
+    });
+
+    rl.on('close', (line) => {
+        for (let host of hostsFileLines) {
+        	hostLines += `${host}\n`;
+        }
+
+        fs.writeFile(hostsFile, hostLines, function (err) {
+          if (err) throw err;
+        });
+
+        restartNetwork();
+    });
+}
+
+var writeToHosts = function writeToHosts() {
+
+    fs.readFile('sites.ini', 'utf8', function (err,data) {
+      if (err) throw err;
+      const sites = data.match(/[^=]+$/)[0].split(', ');
+
+      hostLines += startToken + '\n';
+
+      for (let site of sites) {
+      	hostLines += `127.0.0.1 ${site}\n`;
+      	hostLines += `127.0.0.1 www.${site}\n`;
+      }
+
+      hostLines += endToken + '\n';
+
+      fs.appendFile(hostsFile, hostLines, function (err) {
+        if (err) throw err;
+      });
+    });
+}
+
+gsd(param);
